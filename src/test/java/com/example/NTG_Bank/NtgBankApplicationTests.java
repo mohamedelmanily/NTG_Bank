c69cb5309387cf53af66ace9043eb0e8036962d7
Mohamed Reda <mohamedreda@ntgclarity.com>
2025-01-27 17:44:42 +0200

    first commit

---
+++ src/test/java/com/example/NTG_Bank/NtgBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NTG_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtgBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
